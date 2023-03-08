59b02cd7b34b7f3c187728f70451f86b122aac6d
Zikri Mansyursyah <86043071+zikrimansyursyah@users.noreply.github.com>
2023-03-09 03:39:10 +0700

    hotfix bug production

--- app/manajemen-siswa/[id]/page.js
+++ app/manajemen-siswa/[id]/page.js
@@ -10,7 +10,7 @@ const DynamicUserTemplate = dynamic(() => import("./client"), { ssr: false, load
 export default async function Page({ params }) {
   let data = null;
   const { id } = params;
-  await httpCall("POST", "http://" + headers().get("host") + API.USERS_GET_DETAIL_SISWA, { id }, { cookie: headers().get("cookie") })
+  await httpCall("POST", "http://" + (headers().get("host").includes("localhost") ? "127.0.0.1:3000" : headers().get("host")) + API.USERS_GET_DETAIL_SISWA, { id }, { cookie: headers().get("cookie") })
     .then((res) => {
       if (res.status === 200) {
         data = res.data;
@@ -29,7 +29,7 @@ export default async function Page({ params }) {
 export async function generateMetadata({ params }) {
   let title = "Siswa";
   const { id } = params;
-  await httpCall("POST", "http://" + headers().get("host") + API.USERS_GET_NAMA_SISWA, { id }, { cookie: headers().get("cookie") })
+  await httpCall("POST", "http://" + (headers().get("host").includes("localhost") ? "127.0.0.1:3000" : headers().get("host")) + API.USERS_GET_NAMA_SISWA, { id }, { cookie: headers().get("cookie") })
     .then((res) => {
       if (res.status === 200) {
         title = res.data.nama;
