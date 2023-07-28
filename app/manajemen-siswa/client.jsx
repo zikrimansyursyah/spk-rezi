31958c6e415d0d1d32bcb80605459cf1bf3ab12c
Zikri Mansyursyah <86043071+zikrimansyursyah@users.noreply.github.com>
2023-07-29 00:08:36 +0700

    add: import data from excel

--- app/manajemen-siswa/client.jsx
+++ app/manajemen-siswa/client.jsx
@@ -2,7 +2,10 @@
 
 import { useContext, useEffect, useState, useRef } from "react";
 import { AppContext } from "@/context";
-import { deleteSiswa, getAllSiswa } from "@/services/user";
+import { addSiswa, deleteSiswa, getAllSiswa } from "@/services/user";
+import Link from "next/link";
+import { ExcelRenderer } from "react-excel-renderer";
+import FileSaver from "file-saver";
 
 // Components
 import { InputText } from "primereact/inputtext";
@@ -10,11 +13,20 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Menu } from "primereact/menu";
 import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
-import Link from "next/link";
+import { Dialog } from "primereact/dialog";
+import { validateSchema } from "@/validation";
+import { mapperRequest, userImportSchema } from "@/validation/auth";
 
 export default function ManajemenSiswa() {
   const { toast, loading } = useContext(AppContext);
   const menuAction = useRef(null);
+  const [visibleDialog, setVisibleDialog] = useState(false);
+  const [visibleDialogResult, setVisibleDialogResult] = useState(false);
+  const [fileName, setFileName] = useState("");
+  const [file, setFile] = useState(null);
+
+  const [successAdd, setSuccessAdd] = useState(0);
+  const [failedAdd, setFailedAdd] = useState(0);
 
   // Filter
   const [search, setSearch] = useState("");
@@ -108,6 +120,124 @@ export default function ManajemenSiswa() {
       });
   };
 
+  const handleDownloadTemplate = () => {
+    FileSaver.saveAs("/template.xlsx", "Template Import Data Siswa.xlsx");
+  };
+
+  const handleUploadFile = (e) => {
+    if (!e.target.files || !e.target.files[0]) return;
+
+    const tempFile = e.target.files[0];
+    const acceptFile = [".csv", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-excel"];
+
+    if (!acceptFile.includes(tempFile.type)) {
+      return toast.current.show({ severity: "warn", summary: "Gagal Upload", detail: "Tipe file harus berformat excel" });
+    }
+    setFile(tempFile);
+    setFileName(e.target.value);
+  };
+
+  const handleProcessData = async () => {
+    if (!file) {
+      return toast.current.show({ severity: "warn", summary: "Gagal Upload", detail: "Kesalahan pada saat membaca file, harap muat ulang halaman" });
+    }
+
+    let column, data;
+
+    await ExcelRenderer(file, (err, resp) => {
+      if (err) {
+        return toast.current.show({ severity: "warn", summary: "Gagal Proses", detail: err });
+      }
+
+      column = resp.rows[0];
+      data = (resp.rows || []).filter((v, i) => {
+        if (i !== 0 && Array.isArray(v) && v.length > 1) {
+          return v;
+        }
+      });
+    });
+
+    let dataList = [];
+    loading({ text: `Kami sedang menambah 0/${data.length} data`, visible: true });
+    for (const dt of data) {
+      let temp = {};
+      for (let i = 0; i < column.length; i++) {
+        if (column[i] === "Username") {
+          temp[column[i]] = dt[i].toLowerCase();
+        } else {
+          temp[column[i]] = dt[i];
+        }
+      }
+      dataList.push(temp);
+    }
+
+    let payload = [];
+    for (const e of dataList) {
+      const res = validateSchema(userImportSchema, e);
+
+      if (res._error) {
+        return toast.current.show({ severity: "warn", summary: "Data Tidak Sesuai", detail: `${res.message} pada data ${e["Nama Siswa"]} (${e["NISN"]})` });
+      }
+
+      let temp = {};
+      for (const dt of Object.keys(res) || []) {
+        temp[mapperRequest[dt]] = ["is_ayah_bekerja", "is_ibu_bekerja"].includes(mapperRequest[dt]) ? res[dt] === "Bekerja" : res[dt];
+      }
+      payload.push(temp);
+    }
+
+    let successInsert = 0;
+    let failedInsert = 0;
+    for (let i = 0; i < payload.length; i++) {
+      let isError = false;
+      await addSiswa(payload[i])
+        .then((res) => {
+          if (res.status !== 200) {
+            failedInsert++;
+            toast.current.show({
+              severity: "warn",
+              summary: "Gagal menambah data",
+              detail: `${res.message}, pada data ${payload[i].nama} (${payload[i].nisn})`,
+              sticky: true,
+            });
+          } else {
+            successInsert++;
+          }
+        })
+        .catch((err) => {
+          isError = true;
+          toast.current.show({
+            severity: "warn",
+            summary: "Gagal menambah data",
+            detail: err.message,
+            sticky: true,
+          });
+        })
+        .finally(() => {
+          loading({ text: `Kami sedang menambah ${successInsert + failedInsert}/${data.length} data`, visible: true });
+        });
+
+      if (isError) {
+        loading({ text: null, visible: false });
+        break;
+      }
+
+      if (payload.length === i + 1) {
+        loading({ text: null, visible: false });
+        setSuccessAdd(successInsert);
+        setFailedAdd(failedInsert);
+        setVisibleDialogResult(true);
+        setVisibleDialog(false);
+        setFile(null);
+        setFileName("");
+
+        if (successInsert > 0) {
+          getDataSiswa();
+        }
+      }
+    }
+  };
+
   // Template
   const actionBodyTemplate = (data) => {
     return (
@@ -118,7 +248,7 @@ export default function ManajemenSiswa() {
             menuAction.current.toggle(e);
             setSelectedData(data);
           }}
-          className="border border-transparent h-7 w-7 rounded-full hover:shadow hover:border-gray-200 focus:ring focus:ring-blue-100"
+          className="h-7 w-7 rounded-full border border-transparent hover:border-gray-200 hover:shadow focus:ring focus:ring-blue-100"
         >
           <i className="pi pi-ellipsis-v text-sm"></i>
         </button>
@@ -140,29 +270,38 @@ export default function ManajemenSiswa() {
 
   return (
     <div>
-      <div className="p-4 pl-20 md:pl-4 fixed w-full grid grid-cols-2 gap-4 bg-white border-b shadow-[0px_8px_15px_-3px_rgba(0,0,0,0.02)] z-50">
-        <div className="col-span-2 md:col-span-1 flex gap-3">
-          <div className="w-1 h-12 bg-sky-500 rounded-full"></div>
+      <div className="fixed z-50 grid w-full grid-cols-2 gap-4 border-b bg-white p-4 pl-20 shadow-[0px_8px_15px_-3px_rgba(0,0,0,0.02)] md:pl-4">
+        <div className="col-span-2 flex gap-3 md:col-span-1">
+          <div className="h-12 w-1 rounded-full bg-sky-500"></div>
           <div className="flex flex-col">
             <span className="font-semibold">Manajemen User</span>
             <span className="text-sm text-gray-600">Mengelola data siswa di SD Negeri 02 Pakulonan Barat</span>
           </div>
         </div>
-        <div className="col-span-2 md:col-span-1 flex items-center justify-start md:justify-end">
-          <Link href="/manajemen-siswa/tambah" className="w-fit h-fit flex items-center gap-3 bg-[#2293EE] py-2 px-4 rounded-lg hover:bg-[#4da5ed] active:scale-[0.97] focus:ring focus:ring-blue-200">
-            <i className="pi pi-plus text-white text-xs font-semibold"></i>
+        <div className="col-span-2 flex items-center justify-start gap-2 md:col-span-1 md:justify-end">
+          <Link href="/manajemen-siswa/tambah" className="flex h-fit w-fit items-center gap-3 rounded-lg bg-[#2293EE] py-2 px-4 hover:bg-[#4da5ed] focus:ring focus:ring-blue-200 active:scale-[0.97]">
+            <i className="pi pi-plus text-xs font-semibold text-white"></i>
             <span className="text-sm font-medium text-white">Tambah Siswa</span>
           </Link>
+          <button
+            onClick={() => {
+              setVisibleDialog(true);
+            }}
+            className="flex h-fit w-fit items-center gap-3 rounded-lg bg-[#2293EE] py-2 px-4 hover:bg-[#4da5ed] focus:ring focus:ring-blue-200 active:scale-[0.97]"
+          >
+            <i className="pi pi-file-excel text-xs font-semibold text-white"></i>
+            <span className="text-sm font-medium text-white">Import Data Siswa</span>
+          </button>
         </div>
       </div>
       <div className="px-4 pb-4 pt-40 md:pt-24">
-        <div className="bg-white w-full mt-1 rounded-lg border shadow-[0px_8px_15px_-3px_rgba(0,0,0,0.001)] flex flex-col">
-          <div className="py-2 px-4 flex items-center gap-2">
+        <div className="mt-1 flex w-full flex-col rounded-lg border bg-white shadow-[0px_8px_15px_-3px_rgba(0,0,0,0.001)]">
+          <div className="flex items-center gap-2 py-2 px-4">
             <i className="pi pi-filter-fill text-[1rem] text-yellow-300"></i>
             <span className="font-medium">Filter</span>
           </div>
           <span className="border-b"></span>
-          <div className="py-4 px-4 flex justify-between items-center">
+          <div className="flex items-center justify-between py-4 px-4">
             <div></div>
             <div className="flex">
               <span className="p-input-icon-left">
@@ -176,13 +315,13 @@ export default function ManajemenSiswa() {
                   className="p-inputtext-sm rounded-tr-none rounded-br-none"
                 />
               </span>
-              <button onClick={getDataSiswa} className="bg-[#2293EE] py-2 px-4 rounded-tr-lg rounded-br-lg hover:bg-[#4da5ed] active:scale-[0.97] focus:ring focus:ring-blue-200">
+              <button onClick={getDataSiswa} className="rounded-tr-lg rounded-br-lg bg-[#2293EE] py-2 px-4 hover:bg-[#4da5ed] focus:ring focus:ring-blue-200 active:scale-[0.97]">
                 <i className="pi pi-search text-white"></i>
               </button>
             </div>
           </div>
         </div>
-        <div className="bg-white w-full mt-4 p-5 rounded-lg border shadow-[0px_8px_15px_-3px_rgba(0,0,0,0.001)]">
+        <div className="mt-4 w-full rounded-lg border bg-white p-5 shadow-[0px_8px_15px_-3px_rgba(0,0,0,0.001)]">
           <DataTable
             showGridlines
             responsiveLayout="stack"
@@ -213,6 +352,48 @@ export default function ManajemenSiswa() {
         </div>
       </div>
       <ConfirmDialog />
+      <Dialog
+        header="Import Data Siswa"
+        visible={visibleDialog}
+        style={{ width: "40vw" }}
+        breakpoints={{ "960px": "50vw", "641px": "100vw" }}
+        onHide={() => {
+          setVisibleDialog(false);
+          setFile(null);
+          setFileName("");
+        }}
+      >
+        <div className="mb-4 flex">
+          <InputText placeholder="Pilih file data" type="file" value={fileName} onChange={handleUploadFile} className="p-inputtext-sm rounded-tr-none rounded-br-none" />
+          <button
+            onClick={handleProcessData}
+            disabled={!file}
+            className="flex flex-grow items-center justify-center rounded-r-lg bg-[#2293EE] py-2 px-4 hover:bg-[#4da5ed] focus:ring focus:ring-blue-200 active:scale-[0.97]"
+          >
+            <span className="text-sm font-medium text-white">Proses File</span>
+          </button>
+        </div>
+        <small>
+          <span className="text-red-500">*</span> penambahan data siswa menggunakan fitur import harus sesuai dengan template yang telah disediakan
+          <span onClick={handleDownloadTemplate} className="ml-1 cursor-pointer underline">
+            Unduh file template Import Data Siswa
+          </span>
+        </small>
+      </Dialog>
+      <Dialog
+        header="Hasil Import Data Siswa"
+        visible={visibleDialogResult}
+        style={{ width: "40vw" }}
+        breakpoints={{ "960px": "50vw", "641px": "100vw" }}
+        onHide={() => {
+          setVisibleDialogResult(false);
+        }}
+      >
+        <div className="flex flex-col gap-2">
+          <span className="rounded-lg bg-green-100 px-4 py-2 text-lg font-medium">Berhasil : {successAdd}</span>
+          <span className="rounded-lg bg-red-100 px-4 py-2 text-lg font-medium">Gagal : {failedAdd}</span>
+        </div>
+      </Dialog>
     </div>
   );
 }
--- validation/auth.js
+++ validation/auth.js
@@ -29,3 +29,63 @@ export const userSchema = yup.object().shape({
   jenis_pekerjaan_ibu: yup.string().optional(),
   pendapatan_perbulan_ibu: yup.number().optional(),
 });
+
+export const userImportSchema = yup.object().shape({
+  NISN: yup.string().required("nisn harus diisi"),
+  "Nomor Induk Sekolah": yup.string().required("nomor induk sekolah harus diisi"),
+  "Tingkat Kelas": yup.string().required("tingkat kelas harus dipilih 1 - 6"),
+  Username: yup.string().min(4, "username minimal 4 digit/karakter").required("username harus diisi"),
+  Password: yup.string().min(6, "password minimal 6 karakter").required("password harus diisi"),
+  "Nama Siswa": yup.string().min(3, "nama siswa minimal 3 karakter").required("nama siswa harus diisi"),
+  "Tempat Lahir": yup.string().min(3, "tempat lahir minimal 3 karakter").required("tempat lahir harus diisi"),
+  "Tanggal Lahir": yup.string().required("tangal lahir harus diisi"),
+  "Jenis Kelamin": yup
+    .string()
+    .matches(/^(Laki - Laki|Perempuan)$/i, 'Jenis Kelamin hanya "Laki - Laki" / "Perempuan"')
+    .required("jenis kelamin harus diisi"),
+  Alamat: yup.string().min(5, "alamat minimal 5 karakter").required("alamat harus diisi"),
+  "Nomor Telepon": yup.string().min(7, "nomor telepon minimal 7 karakter").required("nomor telepon harus diisi"),
+  "Status Tempat Tinggal": yup
+    .string()
+    .matches(/^(Rumah Kontrak|Rumah Pribadi)$/i, 'Status Tempat Tinggal hanya "Rumah Kontrak" / "Rumah Pribadi"')
+    .required("status tempat tinggal harus diisi"),
+  "Jumlah Saudara Kandung": yup.number().required("jumlah saudara kandung harus diisi"),
+  "Status Pekerjaan Ayah": yup
+    .string()
+    .matches(/^(Bekerja|Tidak Bekerja)$/i, 'Status Pekerjaan hanya "Bekerja" / "Tidak Bekerja"')
+    .required("status pekerjaan harus diisi"),
+  "Nama Ayah": yup.string().min(3, "nama ayah minimal 3 karakter").required("nama ayah harus diisi"),
+  "Jenis Pekerjaan Ayah": yup.string().optional(),
+  "Pendapatan Ayah Perbulan": yup.number().optional(),
+  "Status Pekerjaan Ibu": yup
+    .string()
+    .matches(/^(Bekerja|Tidak Bekerja)$/i, 'Status Pekerjaan hanya "Bekerja" / "Tidak Bekerja"')
+    .required("status pekerjaan harus diisi"),
+  "Nama Ibu": yup.string().min(3, "nama ibu minimal 3 karakter").required("nama ibu harus diisi"),
+  "Jenis Pekerjaan Ibu": yup.string().optional(),
+  "Pendapatan Ibu Perbulan": yup.number().optional(),
+});
+
+export const mapperRequest = {
+  NISN: "nisn",
+  "Nomor Induk Sekolah": "no_induk_sekolah",
+  "Tingkat Kelas": "tingkat_kelas",
+  Username: "username",
+  Password: "password",
+  "Nama Siswa": "nama",
+  "Tempat Lahir": "tempat_lahir",
+  "Tanggal Lahir": "tanggal_lahir",
+  "Jenis Kelamin": "jenis_kelamin",
+  Alamat: "alamat",
+  "Nomor Telepon": "no_telp",
+  "Status Tempat Tinggal": "status_tempat_tinggal",
+  "Jumlah Saudara Kandung": "jumlah_anggota_keluarga",
+  "Status Pekerjaan Ayah": "is_ayah_bekerja",
+  "Nama Ayah": "nama_ayah",
+  "Jenis Pekerjaan Ayah": "jenis_pekerjaan_ayah",
+  "Pendapatan Ayah Perbulan": "pendapatan_perbulan_ayah",
+  "Status Pekerjaan Ibu": "is_ibu_bekerja",
+  "Nama Ibu": "nama_ibu",
+  "Jenis Pekerjaan Ibu": "jenis_pekerjaan_ibu",
+  "Pendapatan Ibu Perbulan": "pendapatan_perbulan_ibu",
+};
